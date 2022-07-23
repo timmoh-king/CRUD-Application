6e38f19c54859e50e3e3343f4b634816fffd7e2b
timmoh-king <timothykariuki43@gmail.com>
2022-07-24 02:06:10 +0300

    installed dependancies

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,20 @@
  * @Author: Your name
  * @Date:   2022-07-23 00:57:30
  * @Last Modified by:   Your name
- * @Last Modified time: 2022-07-23 11:32:54
+ * @Last Modified time: 2022-07-24 01:54:09
  */
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
+    colors:{
+      tododark: '#000000',
+      tododarkfade: '#373737',
+      todoblue: '#0D99FF',
+      todoorange: '#FA4A2D',
+      todogreen: '#9CC87F',
+      todobackground: '#FAFAFA'
+    },
     extend: {},
   },
   plugins: [],
